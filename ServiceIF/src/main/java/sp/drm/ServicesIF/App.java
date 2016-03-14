7432e313da772336d9980dae1454925e660c2cfa
samwise3 <samueltobey@gmail.com>
2016-03-13 18:26:08 -0600

    fix pox bug and IF template

---
+++ ServiceIF/src/main/java/sp/drm/ServicesIF/App.java
@@ -0,0 +1,13 @@
+package sp.drm.ServicesIF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
