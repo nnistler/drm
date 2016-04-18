c8da4a81fbdb1cb83e57963f5678ffb5b3a421c5
Michael Brughelli <michael.brughelli@colorado.edu>
2016-04-18 17:59:45 -0600

    Color Mixer Demo fix

--- colorMixer/src/main/java/sp/drm/colorMixer/ColorMixer.java
+++ colorMixer/src/main/java/sp/drm/colorMixer/ColorMixer.java
@@ -1,57 +1,54 @@
 package sp.drm.colorMixer;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import java.awt.Graphics;
-import java.awt.event.WindowEvent;
-import java.awt.Color;
 import sp.drm.ServicesIF.ColorIF;
 
 @SuppressWarnings("serial")
-public class ColorMixer extends JPanel implements ColorIF
-{
-	private JFrame frame = null;
-	private Color rgbaVal;
-	public ColorMixer() 
-	{
-		
-		super();
-		frame = new JFrame();
-		rgbaVal = new Color(0,0,0);
+public class ColorMixer extends JPanel implements ColorIF {
+    private JFrame frame = null;
+    private Color rgbaVal;
 
-		this.setBackground(rgbaVal);
-	}
-	
-	public Color getColor(){
-		return this.rgbaVal;
-	}
-	
-	public void paintComponent(Graphics graphics) {
-		super.paintComponent(graphics);
-	}
+    public ColorMixer() {
 
-	public void mix(ColorIF c1, ColorIF c2) {
-		int r = (c1.getColor().getRed() + c2.getColor().getRed()) / 2;
-		int g = (c1.getColor().getGreen() + c2.getColor().getGreen()) / 2;
-		int b = (c1.getColor().getBlue() + c2.getColor().getBlue()) / 2;
-		rgbaVal = new Color(r,g,b);
-		this.setBackground(rgbaVal);
-	}
-	
-	public void draw()
-	{
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.add(this);
-		frame.setSize(1280,720);
-		frame.setVisible(true);
-	}
+	super();
+	frame = new JFrame();
+	rgbaVal = new Color(0, 0, 0);
 
-	public void close()
-	{
-		if(frame != null)
-		{
-			frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-		}
+	this.setBackground(rgbaVal);
+    }
+
+    public Color getColor() {
+	return this.rgbaVal;
+    }
+
+    public void paintComponent(Graphics graphics) {
+	super.paintComponent(graphics);
+    }
+
+    public void mix(ColorIF c1, ColorIF c2) {
+	int r = (c1.getColor().getRed() + c2.getColor().getRed()) / 2;
+	int g = (c1.getColor().getGreen() + c2.getColor().getGreen()) / 2;
+	int b = (c1.getColor().getBlue() + c2.getColor().getBlue()) / 2;
+	rgbaVal = new Color(r, g, b);
+	this.setBackground(rgbaVal);
+    }
+
+    public void draw() {
+	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	frame.add(this);
+	frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	frame.setVisible(true);
+    }
+
+    public void close() {
+	if (frame != null) {
+	    frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	}
+    }
 }
