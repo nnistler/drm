34de5111802ebc2dfd4d8f75c85aca1053907d23
samwise3 <samueltobey@gmail.com>
2016-04-27 16:28:44 -0600

    remove ant stuff

--- simpleGraphicService/src/com/rtn/drm/simpleGraphics/ISimpleGraphics.java
+++
@@ -1,9 +0,0 @@
-package com.rtn.drm.simpleGraphics;
-
-import java.awt.Graphics;
-
-public interface ISimpleGraphics {
-	public void paintComponent(Graphics graphics);
-	public void draw();
-	public void close();
-}
