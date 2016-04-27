062f991d2d6ace13a769320dfbc8868977f872ac
samwise3 <samueltobey@gmail.com>
2016-04-27 16:19:34 -0600

    Bundle Starter

---
+++ bundleStarter/src/com/rtn/drm/bundleStarter/impl/Activator.java
@@ -0,0 +1,43 @@
+package com.rtn.drm.bundleStarter.impl;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+// A simple bundle that registers Long Services and 
+//  retrieves their service references.
+public class Activator implements BundleActivator{
+	
+	public static BundleContext bundleContext = null;
+	private Bundle bundle = null;
+	private String bundleToStart = "simple_graphics";
+	private String simpleGraphicsPath = "file:C:\\Users\\Samuel Tobey\\Desktop\\Fall 2015\\csci_4308_senior_project\\drm\\services\\simpleGraphicService\\build\\simple_graphics.jar";
+	
+	public void start(BundleContext bundleContext) throws Exception {
+		System.out.println("Starting bundleStarter.");
+		Activator.bundleContext = bundleContext;
+//		OLD WAY OF FINDING BUNDLE. DUMB.//		Bundle[] bl = bundleContext.getBundles();
+//		for(int i=0; i<bl.length; i++) {
+//			if(bl[i].getLocation().contains(bundleToStart)) {
+//				bundle = bl[i];
+//				break;
+//			}
+//		}
+		
+		bundle = bundleContext.installBundle(simpleGraphicsPath);
+		if(bundle != null) {
+			bundle.start();
+		}
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("Stopping bundleStarter.");
+		if(bundle != null) {
+			bundle.stop(); // if we stop simpleGraphics and uninstall it from gui, then stop this bundle, ERROR occurs.
+//			TODO: Add service listener to fix error noted above.  Should this bundle should know when simpleGraphics is uninstalled or stopped. 
+			bundle.uninstall();
+		}
+		Activator.bundleContext = null;
+	}
+}
