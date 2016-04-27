ed2d75d6e8a93a464ead5d6aa686b7467964642d
rowells <rowells@colorado.edu>
2016-04-27 17:11:17 -0600

    Added documentation

--- colorTracker/src/main/java/sp/drm/colorTracker/ColorTrackerCustomizer.java
+++ colorTracker/src/main/java/sp/drm/colorTracker/ColorTrackerCustomizer.java
@@ -3,8 +3,9 @@ package sp.drm.colorTracker;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.Filter;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
-
+import org.osgi.framework.ServiceEvent;
 import sp.drm.ServicesIF.ColorIF;
 
 public class ColorTrackerCustomizer<ColoIF,ColorIF> implements ServiceTrackerCustomizer{
@@ -14,15 +15,25 @@ public class ColorTrackerCustomizer<ColoIF,ColorIF> implements ServiceTrackerCus
 	public ColorTrackerCustomizer(BundleContext bundleContext) {
 		this.bundleContext = bundleContext;
 	}
-	
+
 	public Object addingService(ServiceReference serviceReference) {
 		log("Adding Color Service");
 		Object colorService = bundleContext.getService(serviceReference);
 		return colorService;
 	}
 
-	public void modifiedService(ServiceReference serviceReference, Object service) {
-		// TODO update/reinitialize some service
+	public void modifiedService(ServiceReference serviceReference, Object someService) {
+		ServiceEvent myService = null;
+		myService = (ServiceEvent) someService; 
+		
+		switch (myService.getType()) {
+		case ServiceEvent.MODIFIED:
+			log("Service modified");
+			//x = (ColorIF) Activator.bundleContext.getService(myService.getServiceReference());
+			break;	
+		default:
+			break;
+		}
 	}
 
 	public void removedService(ServiceReference serviceReference, Object service) {
